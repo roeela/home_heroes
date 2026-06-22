db6cf5bd8ddedbee70a963146714b701d3d4c8bb
roeela <roeela1@gmail.com>
2026-06-22 14:01:17 +0300

    wip

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.example.homeheroes.home_heroes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
